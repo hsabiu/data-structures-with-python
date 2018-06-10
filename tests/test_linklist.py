4aa2d4691861775683afac4c162cbf4a74eeebb8
Habib Sabiu <habibado2006@gmail.com>
2018-06-10 00:56:06 -0600

    Added test cases template for linklist class

---
+++ tests/test_linklist.py
@@ -0,0 +1,46 @@
+import unittest
+from LinkList import SinglyLinkList, DoublyLinkList
+
+
+class TestSinglyLinkList(unittest.TestCase):
+    # Class to test the implementation of a singly link list
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_size(self):
+        pass
+
+    def test_search(self):
+        pass
+
+
+class TestDoublyLinkList(unittest.TestCase):
+    # Class to test the implementation of a singly link list
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_insert(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_size(self):
+        pass
+
+    def test_search(self):
+        pass
