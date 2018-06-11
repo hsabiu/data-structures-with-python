cb9ab11a6bb34029c0bbbc71ab2806107444839e
Habib Sabiu <habibado2006@gmail.com>
2018-06-10 19:26:25 -0600

    Added test cases for testing the correctness of singly linked list implementation

--- tests/test_linklist.py
+++ tests/test_linklist.py
@@ -6,41 +6,38 @@ class TestSinglyLinkList(unittest.TestCase):
     # Class to test the implementation of a singly link list
 
     def setUp(self):
-        pass
+        self.link_list = SinglyLinkList()
+        self.link_list.insert(1)
+        self.link_list.insert(2)
+        self.link_list.insert(3)
 
     def tearDown(self):
-        pass
+        self.link_list = None
 
     def test_insert(self):
-        pass
+        self.link_list.insert(4)
+        self.assertEqual(self.link_list.head.data, 4)
 
-    def test_delete(self):
-        pass
+    def test_correct_delete(self):
+        self.link_list.delete(3)
+        self.assertEqual(self.link_list.head.data, 2)
+
+    def test_wrong_delete(self):
+        self.assertRaises(ValueError, self.link_list.delete, 4)
+
+    def test_correct_search(self):
+        try:
+            self.link_list.search(1)
+        except ValueError:
+            self.fail("search raised ValueError")
+
+    def test_wrong_search(self):
+        self.assertRaises(ValueError, self.link_list.search, 4)
 
     def test_size(self):
-        pass
-
-    def test_search(self):
-        pass
+        self.assertEqual(self.link_list.size(), 3)
 
 
 class TestDoublyLinkList(unittest.TestCase):
     # Class to test the implementation of a singly link list
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_insert(self):
-        pass
-
-    def test_delete(self):
-        pass
-
-    def test_size(self):
-        pass
-
-    def test_search(self):
-        pass
+    pass
